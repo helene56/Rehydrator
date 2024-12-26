1c7c82418437d8490a16aa7aa8fdb220c781bdb1
helene <helene.maja@hotmail.dk>
2024-12-26 23:28:49 +0100

    add pio program

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
 #include <stdio.h>
+// pio program
+#include "build/rgbLed.pio.h"
 
 int main()
 {
     stdio_init_all();
+
+    uint const rgb_din {16};
+
+    PIO pio = pio0;
+    int sm = 0;
+    uint offset = pio_add_program(pio, &rgbLed_program);
+    rgbLed_program_init(pio, sm, offset, rgb_din, 800000);
+
     uint const led {1};
     gpio_init(led);
     gpio_set_dir(led, GPIO_OUT);
