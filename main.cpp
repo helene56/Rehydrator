607047ad04a8583fc18faa08960ae84af08ccfd4
helene <helene.maja@hotmail.dk>
2024-12-26 22:55:44 +0100

    add a test program, flashing a led

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
-
+#include <stdio.h>
 
 int main()
 {
     stdio_init_all();
-
-    while (true) {
+    uint const led {1};
+    gpio_init(led);
+    gpio_set_dir(led, GPIO_OUT);
+    while (true)
+    {
         printf("Hello, world!\n");
+        gpio_put(led, 1);
+        sleep_ms(1000);
+        gpio_put(led, 0);
         sleep_ms(1000);
     }
 }
