455b4957cd0b4591121287980d79f5ad7c92b17f
helene <helene.maja@hotmail.dk>
2024-12-26 23:44:41 +0100

    convert const to constexpr

--- src/hydrator.cpp
+++ src/hydrator.cpp
@@ -3,7 +3,7 @@
 
 double read_moisture() 
 {
-    const double conversion_factor = 3.3f / (1 << 12); // Convert to voltage
+    constexpr double conversion_factor = 3.3f / (1 << 12); // Convert to voltage
     double voltage;
     uint16_t result = adc_read(); // Read raw sensor value from ADC0
     voltage = result * conversion_factor; // Convert raw value to voltage
