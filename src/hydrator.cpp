6ed98f67b11cc71256c4e57f6834468155decc1e
helene <helene.maja@hotmail.dk>
2024-12-27 13:23:20 +0100

    update DRY and sensor pin to correct ADC0

--- src/hydrator.cpp
+++ src/hydrator.cpp
@@ -1,10 +1,11 @@
 // purpose of this is to read from the Capaltive Soil Moisture Sensor
 #include "hardware/adc.h"
+#include <stdio.h>
 
 namespace moisture
 {
     constexpr double DRY {2.20f};
-    constexpr uint sensor {0};
+    constexpr uint sensor {26}; // ADC0 attached
 
 } // namespace moisture
 
@@ -21,8 +22,10 @@ double read_moisture()
     constexpr double conversion_factor {3.3f / (1 << 12)}; // Convert to voltage
     uint16_t result {adc_read()};                          // Read raw sensor value from ADC0
     // return the voltage
+    printf("result voltage: %f\n", result * conversion_factor);
     return result * conversion_factor;
 }
+
 
 bool test_moisture()
 {
